304722a3c5e40050f585c7df43d546d799036910
jaewon0913 <jaewon9937@gmail.com>
2019-10-26 18:07:24 +0900

    2장

---
+++ src/main/java/com/springboot/webservice/web/WebRestController.java
@@ -0,0 +1,28 @@
+package com.springboot.webservice.web;
+
+import com.springboot.webservice.domain.posts.PostsRepository;
+import com.springboot.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
+
+
+}
---
+++ src/test/java/com/springboot/webservice/domain/posts/PostsRepositoryTestTest.java
@@ -0,0 +1,8 @@
+package com.springboot.webservice.domain.posts;
+
+import static org.junit.Assert.*;
+
+
+public class PostsRepositoryTestTest {
+
+}
