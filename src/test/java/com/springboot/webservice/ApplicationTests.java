74907524cabad0e8dda3290da2a86d2522649e05
jaewon0913 <jaewon9937@gmail.com>
2019-11-02 17:10:55 +0900

    5장

--- src/test/java/com/springboot/webservice/ApplicationTests.java
+++ src/test/java/com/springboot/webservice/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
 
     @Test
     void contextLoads() {
