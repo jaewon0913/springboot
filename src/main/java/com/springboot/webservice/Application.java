5fd52b4d394fbae1174892684ceedbd7d028362d
jaewon0913 <jaewon9937@gmail.com>
2019-12-28 17:05:12 +0900

    SpringBoot 1.5->2 / 로그인 추가

--- src/main/java/com/springboot/webservice/Application.java
+++ src/main/java/com/springboot/webservice/Application.java
@@ -15,5 +15,4 @@ public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).properties(APPLICATION_LOCATIONS).run(args);
     }
-
 }
